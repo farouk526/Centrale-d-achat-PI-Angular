3b8343011f589a8f963f3739019fdfa63cc6c014
AichaGasrii <aicha.gasri@gmail.com>
2023-04-25 14:36:05 +0100

    Template+userCRUD

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AdminModule } from "./main/Modules/Admin/admin.module";
+
+import { ProfileModule } from "./main/Modules/profile/profile.module";
+
+
+
+const routes: Routes = [
+  {
+    path: "pages",
+    loadChildren: () =>
+      import("./main/pages/pages.module").then((m) => m.PagesModule),
+  },
+  
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full",
+  },
+
+  {
+    path: "**",
+    redirectTo: "/pages/miscellaneous/error", //Error 404 - Page not found
+  },
+  
+  
+];
+
+@NgModule({
+  imports: [ProfileModule,AdminModule,RouterModule.forRoot(routes,{onSameUrlNavigation: 'reload'})],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
